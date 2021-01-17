eef635e8cdfdc8a0d1749f3c419090097128a7d4
andrejstrashun <andrejstrashun@gmail.com>
2021-01-17 15:33:38 +0200

    first

---
+++ src/Nav/index.js
@@ -0,0 +1,10 @@
+// src/Nav/index.js
+import React from 'react';
+import './Nav.css';
+import logo from './logo.png';
+const Nav = () => (
+  <nav>
+    <img src={logo} alt="" />
+  </nav>
+);
+export default Nav;
